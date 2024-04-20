689bf41135e15421437785cc042ba6fada791409
DavidL0509 <152031021+DavidL0509@users.noreply.github.com>
2024-04-20 22:50:31 +0200

    mapbox

--- src/components/app/transitMapApplication.tsx
+++ src/components/app/transitMapApplication.tsx
@@ -12,10 +12,17 @@ import { Draw } from "ol/interaction";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { Circle, Fill, Icon, Stroke, Style } from "ol/style";
+// @ts-ignore
+import { MapboxVectorLayer } from "ol-mapbox-style";
 
 useGeographic();
 
-const backgroundLayer = new TileLayer({ source: new OSM() });
+const openStreetMapLayer = new TileLayer({ source: new OSM() });
+const vectorTileLayer = new MapboxVectorLayer({
+  styleUrl: "mapbox://styles/mapbox/dark-v9",
+  accessToken:
+    "pk.eyJ1IjoiYWRtaW5kYXZpZCIsImEiOiJjbHY4azBndXQwbXJtMmpycnF3ZXk4dTg1In0.C50VV23YkRWTzZZ357xaew",
+});
 const map = new Map({
   view: new View({ center: [10, 63], zoom: 8 }),
 });
@@ -54,7 +61,7 @@ const ferryStyle = [
 export function TransitMapApplication() {
   const { vehicleLayer, vehicleTrailLayer } = useVehicleLayer();
   const layers = useMemo(
-    () => [backgroundLayer, vehicleTrailLayer, vehicleLayer, drawingLayer],
+    () => [vectorTileLayer, vehicleTrailLayer, vehicleLayer, drawingLayer],
     [vehicleLayer, vehicleLayer],
   );
   useEffect(() => map.setLayers(layers), [layers]);
